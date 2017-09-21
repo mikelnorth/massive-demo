bed245d64534f3215ea087ca2d3772fcc36e63b6
Mikel North <mikel.north@gmail.com>
2017-09-21 12:31:08 -0600

    postgress conecting to node

--- server.js
+++ server.js
@@ -1,23 +1,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require("massive")
+const React = require('react');
+const {renderToString} = require('react-dom/server');
+
+
+const connectionString = ('postgres://mbbmsgisnpudci:54d0528ed0c187f6fef4aa05f6763e9ae69beb8619c1f596e94034ed41d9cdbe@ec2-54-235-90-125.compute-1.amazonaws.com:5432/d95bu04qmknjbe?ssl=true')
 
 const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
 
+const InjuriesList = ({injuries}) => 
+<div>
+  {injuries.map(injury => <div key={injury.id}>{injury.name}</div>)}
+</div>
+
 app.get('/', (req, res) => {
-  res.send('massive-demo');
+  const db = req.app.get('db');
+  db.getAllInjuries().then(injuries => {
+    const html = renderToString(
+      <InjuriesList injuries={injuries} />
+    );
+    res.send(html);
+  });
 });
+
 
 app.get('/incidents', (req, res) => {
-  res.send([]);
+  const db = req.app.get("db")
+  const state = req.query.state;
+  console.log(state)
+
+  if (state) {
+    db.getIncidentsByState([state]).then(incidents => {
+      res.send(incidents)
+    })
+  }
+  else {
+    db.getAllIncidents().then(incidents => {
+      res.send(incidents);
+    })
+  }
 });
+
 
 app.post('/incidents', (req, res) => {
-  res.send({id: 123});
+  const incident = req.body;
+  const db = req.app.get('db');
+
+  db.createIncidents([incident.state, incident.injuryId, incident.causeId]).then( results => {
+    res.send(results[0]);
+  }) 
 });
 
-app.listen(port, () => {
-  console.log('Started server on port', port);
-});
+
+massive(connectionString).then(db => {
+  app.set('db', db)
+  app.listen(port, () => {
+    console.log('Server conected on port', port);
+  });
+})
